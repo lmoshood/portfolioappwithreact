8bfe0e8e290e8c9b2d4535fcde147bed7438b934
DESKTOP-GRHBJ2K\Moshood Lawal <moshood.lawal@thepfs.biz>
2023-08-16 09:36:05 +0100

    update index
    We remove unused components

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Clock from './Components/MyClock';
-import Counter from './Components/Counter';
+
 
 
 
@@ -11,8 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <Clock/> */}
-    {/* <Counter/> */}
+   
 
   </React.StrictMode>
 );
